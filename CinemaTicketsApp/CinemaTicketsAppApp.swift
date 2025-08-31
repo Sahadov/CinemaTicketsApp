3cc737d9156b284902445dace8309cb52c6923a5
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-08-31 18:23:15 -0300

    Create skeleton project

---
+++ CinemaTicketsApp/CinemaTicketsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaTicketsAppApp.swift
+//  CinemaTicketsApp
+//
+//  Created by Dmitry Volkov on 31/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CinemaTicketsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
